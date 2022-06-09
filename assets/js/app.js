34760bf2f11991e49a342515236464767d0194da
do <do@mission.plus>
2022-06-09 17:31:26 +0700

    up code 6 bai tap

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,37 @@ function popovers() {
   const popovers1 = document.querySelector('#popovers');
   popovers1.classList.toggle('show');
 }
+
+//toasts
+const closeList = document.querySelectorAll(".btn-close");
+const divList = document.querySelectorAll(".toast");
+
+function close() {
+    divList.forEach(() => {
+        closeList.forEach((btn)=>{
+            btn.addEventListener('click',()=>{
+                const div = document.querySelector(".toast");
+                console.log(div);
+                div.classList.toggle('close')
+            })
+        })
+});
+}
+close();
+
+
+function funcToasts(){
+
+  document.querySelector('#notification-toasts').classList.add('show');
+  const myTimeout = setTimeout(() => {
+    document.querySelector('#notification-toasts').classList.remove('show');
+  }, 2000);
+
+}
+
+//end toasts
+
+//scrollspy
+function funcDrop() {
+    document.querySelector('#drop1').classList.toggle('show');
+}
